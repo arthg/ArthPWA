be1d29821d6a1e9ac4ca220efd8346c41b1ec055
Arthur Gorr <art.gorr@gmail.com>
2017-02-18 23:34:29 -0600

    Skeleton harness for weightss service tests

---
+++ src/ArthPWA.Tests/Api/Weights/WeightsServiceTests.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthPWA.Tests.Api.Weights
+{
+    public class WeightsServiceTests
+    {
+    }
+}
