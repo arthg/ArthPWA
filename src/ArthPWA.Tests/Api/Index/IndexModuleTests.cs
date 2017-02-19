089ec4f27a307f722077a4a5a5f188d1ec866afb
Arthur Gorr <art.gorr@gmail.com>
2017-02-18 21:00:34 -0600

    Initial test harness

---
+++ src/ArthPWA.Tests/Api/Index/IndexModuleTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Nancy.Testing;
+using Nancy;
+using SharpTestsEx;
+using ArthPWA.Api.Index;
+
+namespace ArthPWA.Tests.Api.Index
+{
+    [TestFixture]
+    public class IndexModuleTests
+    {
+        private Browser _browser;
+        
+        [SetUp]
+        public void PrepareTestBrowser()
+        {          
+            _browser = new Browser(with => with.Module(new IndexModule()));
+        }
+
+        public sealed class RootUrl : IndexModuleTests
+        {
+            [Test]
+            public void Should_return_status_code_200()
+            {
+                //act
+                var response = _browser.Get("/");
+
+                //assert
+                response.StatusCode.Should().Be.EqualTo(HttpStatusCode.OK);
+            }
+        }
+    }
+}
