4a5e0c31ce1ad803cdcff57bb92a37a5ccc3a719
Arthur Gorr <art.gorr@gmail.com>
2017-02-19 22:09:56 -0600

    Introduce RestSharp test client console app Introducing Polly for retry policy

---
+++ src/RestSharpTestClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RestSharpTestClient
+{
+    public class Program
+    {
+        public static void Main(string[] arg) => new Program().Main();
+
+        private WeightsApiEndpointClient _weightsApiEndpointClient;
+        
+        public void Main()
+        {
+            _weightsApiEndpointClient = new WeightsApiEndpointClient();
+
+            var response = _weightsApiEndpointClient.Ttt().Result;
+
+           
+            Console.WriteLine(response.Content);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ src/RestSharpTestClient/WeightsApiEndpointClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+using System.Threading;
+using System;
+using Polly;
+
+namespace RestSharpTestClient
+{
+    public class WeightsApiEndpointClient
+    {
+        private static Policy retryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(3000, attempt => TimeSpan.FromMilliseconds(100*Math.Pow(2, attempt)));
+
+        public async Task<IRestResponse> Ttt()
+        {
+            return await retryPolicy.ExecuteAsync(() => GetWeights());
+        }
+
+        private static async Task<IRestResponse> GetWeights()
+        {
+            var client = new RestClient("http://localhost:60743");
+            var request = new RestRequest("weights", Method.GET);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            return await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+        }
+    }
+}
